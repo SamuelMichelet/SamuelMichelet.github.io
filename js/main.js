8d066ab30adb81f82fc029006d58be5b106bb47c
Samuel <you@example.com>
2018-09-25 17:16:21 +0200

    animation v1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+const gradient = document.querySelector("body");
 
+function onMouseMove(event) {
+  gradient.style.background = 'radial-gradient(at ' + event.clientX + 'px ' + event.clientY + 'px, rgba(200,200,200,.9) 0, #fff 70%)';
+}
+document.addEventListener("mousemove", onMouseMove);
 
 
